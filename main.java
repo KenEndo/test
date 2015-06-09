37bd293046573d0c521fff7e58ca5dd64d9d6e3e
KenEndo <j70ker2@icloud.com>
2015-06-09 21:05:41 +0900

    first commit

--- main.java
+++ main.java
@@ -1,1 +1,15 @@
+package myapp;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+public class DemoServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("{ \"name\": \"World\" }");
+    }
+}
+
 import java.util.*;
